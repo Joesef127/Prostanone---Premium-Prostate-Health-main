6360b34f24a2f3c64ca3faa572894b4e16a1d08d
joesef127 <adegboladayor@gmail.com>
2026-04-07 18:21:35 +0100

    feat: remove trailing slashes from production CORS origins

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { handle } from "@hono/node-server/vercel";
@@ -15,8 +14,8 @@ app.use(
     origin:
       process.env.NODE_ENV === "production"
         ? [
-            "https://prostanone-premium-prostate-heal-git-b1d987-joesef127s-projects.vercel.app/",
-            "https://prostanone-premium-prostate-health.vercel.app/",
+            "https://prostanone-premium-prostate-heal-git-b1d987-joesef127s-projects.vercel.app",
+            "https://prostanone-premium-prostate-health.vercel.app",
           ]
         : ["http://localhost:3000"],
     credentials: true,
